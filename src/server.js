0a948e7c382f3a22d2dbcd51268ae8d25590da6a
DaviCorreia <davicorreia21@gmail.com>
2020-08-16 00:13:18 -0300

    refatorando e usabdo nunjucks

--- src/server.js
+++ src/server.js
@@ -3,17 +3,25 @@ const server = express()
 
 //configurar pasta publica para importar todo CSS
 server.use(express.static("public"))
+
+//utilizando tamplete engine
+const nunjucks = require("nunjucks")
+
+nunjucks.configure("src/views",{
+    express:server,
+    noCache:true
+})
 //configurar caminhos da minha aplicação
 //pagian inicial
 // req : requisição
 // res : resposta
 
 server.get("/",(req,res)=>{
-    res.sendFile(__dirname+"/views/index.html")
+   return res.render("index.html",{title:"Davi"})
 })
 
 server.get("/create-point",(req,res)=>{
-    res.sendFile(__dirname+"/views/create-point.html")
+   return res.render("create-point.html")
 })
 
 // ligar o servidor
