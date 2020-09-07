d8745f892c8213921e1f27a637bf073d3fed8fd8
DaviCorreia <davicorreia21@gmail.com>
2020-09-07 02:08:26 -0300

    Criando o modal de cadastro concluido e configurando as rotas

--- src/server.js
+++ src/server.js
@@ -59,16 +59,17 @@ server.post("/savepoint",(req,res)=>{
         req.body.address2,
         req.body.state,
         req.body.city,
-        req.body.items,
+        req.body.items
     ]
 
     function afterInsertData(err){
         if (err){
-            return console.log(err)
+            console.log(err)
+            return res.send("Erro no cadastro!")
         }
         console.log("Cadastrado com sucesso")
         console.log(this)
-        return res.send("ok")
+        return res.render("create-point.html",{saved:true})
     }
     db.run(query,values,afterInsertData)
 
@@ -78,9 +79,16 @@ server.post("/savepoint",(req,res)=>{
 
 server.get("/search",(req,res)=>{
 
+    const search = req.query.search
+
+    if(search == ""){
+        //pesquisa vazia
+        return res.render("search-results.html" ,{ total:0})
+    }
+
    // pegar os dados do banco de dados
 
-   db.all (`SELECT * FROM places`,function(err,rows){
+   db.all (`SELECT * FROM places WHERE city LIKE =$'%{search}%'`,function(err,rows){
       if (err){
           return console.log(err)
       }
